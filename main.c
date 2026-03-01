d644ac27c8a620a190cbcc5da2444d23e7e74ce4
demartm <artemis20242511@gmail.com>
2026-03-01 13:07:36 -0500

    Разделил код на функции

--- main.c
+++ main.c
@@ -7,36 +7,66 @@
 2. Уплотнить строку S1, удалив символы, встречающиеся в строке S2.
 */
 
-int main()
-{
-        //char S1[] = "nothingspc"; //Вывод: ningspc
-        //char S2[] = "oth";
+bool fill_Tab(char *S2, bool *Tab) {
 
-        //char S1[] = "somerandom"; //Вывод: smandm
-        //char S2[] = "qwertyuiop";
-
-        //char S1[] = "randomtext"; //Вывод: random
-        //char S2[] = "text";
-
-        char S1[] = "randomtext"; //Вывод: text
-        char S2[] = "random";
-        bool exist[256] = { 0 };
-
-        printf("%s\n", S1);
+    if (S2 && Tab) {
 
         for (int i = 0; S2[i] != '\0'; i++) {
-                exist[S2[i]] = 1;
+            Tab[S2[i]] = 1;
         }
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
 
+bool delete_matches(char *S1, bool *Tab) {
+
+    if (S1 && Tab) {
         int old = 0;
 
         for (int i = 0; S1[i] != '\0'; i++) {
-                if (!exist[S1[i]]) {
-                        S1[old] = S1[i];
+            if (!Tab[S1[i]]) {
+                S1[old] = S1[i];
                 old++;
-                }
+            }
 
         }
         S1[old] = '\0';
-        printf("%s\n", S1);
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+
+int main()
+{
+    //char S1[] = "nothingspc"; //Вывод: ningspc
+    //char S2[] = "oth";
+
+    //char S1[] = "somerandom"; //Вывод: smandm
+    //char S2[] = "qwertyuiop";
+
+    //char S1[] = "randomtext"; //Вывод: random
+    //char S2[] = "text";
+
+    char S1[] = "randomtext"; //Вывод: text
+    char S2[] = "random";
+    bool Tab[256] = { 0 };
+
+    printf("%s\n", S1);
+
+    if (!fill_Tab(S2, Tab)) {
+        printf("Invalid pointer");
+        return 0;
+    }
+    if (!delete_matches(S1, Tab)) {
+        printf("Invalid pointer");
+        return 0;
+    }
+
+    printf("%s\n", S1);
 }
