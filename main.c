0e5b4252bfeddd02da0b10d6ddc9922b4a3f2a12
demartm <artemis20242511@gmail.com>
2026-03-02 15:15:10 -0500

    Удалил функцию fill_tab, добавил больше тестов

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <locale.h>
+#include <wchar.h>
 
 
 /*
@@ -8,22 +10,16 @@
 1. Даны два слова W1 и W2. Составляют ли они анаграмму (т.е. получаются ли друг из друга перестановкой букв)? Пример анаграммы: комар - корма.
 */
 
-bool fill_tab(char *W1,int *Tab) {
-    if (W1 && Tab) {
+bool check_annogram(char* W1, char* W2) {
+
+    if (W1 && W2) {
+        int Tab[256] = { 0 };
+
         for (int i = 0; W1[i] != '\0'; i++) {
-            Tab[W1[i]] += 1;
+            Tab[(unsigned char)W1[i]] += 1;
         }
-        return 1;
-    }
-    else {
-        return 0;
-    }
 
-}
 
-bool check_annogram(char *W1, char *W2, int *Tab) {
-
-    if (W1 && W2 && Tab) {
         bool annogram = true;
 
         if (strlen(W1) != strlen(W2)) {
@@ -32,11 +28,11 @@ bool check_annogram(char *W1, char *W2, int *Tab) {
 
         for (int i = 0; W2[i] != '\0' && annogram; i++) {
 
-            if (!Tab[W2[i]]) {
+            if (!Tab[(unsigned char)W2[i]]) {
                 annogram = false;
             }
             else {
-                Tab[W2[i]] -= 1;
+                Tab[(unsigned char)W2[i]] -= 1;
             }
         }
         return annogram;
@@ -48,28 +44,53 @@ bool check_annogram(char *W1, char *W2, int *Tab) {
 
 int main()
 {
-    //unsigned char W1[] = "qwertyuiop";
-    //unsigned char W2[] = "uiopqwerty"; //Вывод: annogram
+    //Насколько я понял, кириллические символы нормально определяются благодаря устройству Unicode, который не оставляет вариантов для коллизий байтовых значений разных символов
+
+    //char W1[] = "qwertyuiop";
+    //char W2[] = "uiopqwerty"; //Вывод: annogram
 
 
-    //unsigned char W1[] = "noannogram";
-    //unsigned char W2[] = "annogram"; //Вывод: not an annogram
+    //char W1[] = "noannogram";
+    //char W2[] = "annogram"; //Вывод: not an annogram
 
-    //unsigned char W1[] = "randomtcxt";
-    //unsigned char W2[] = "ardnmoettx"; //Вывод: not an annogram
+    //char W1[] = "randomtcxt";
+    // char W2[] = "ardnmoettx"; //Вывод: not an annogram
 
-    unsigned char W1[] = "randomtext";
-    unsigned char W2[] = "ardnmoettx"; //Вывод: annogram
+    char W1[] = "listen";
+    char W2[] = "silent"; //Вывод: annogram
 
-    int Tab[256] = { 0 };
+    //char W1[] = "проверка";
+    //char W2[] = "рпвореак"; //Вывод: annogram
+
+    //char W1[] = "комар";
+    //char W2[] = "корма"; //Вывод: annogram
+
+    //char W1[] = "тест";
+    //char W2[] = "тесто"; //Вывод: not an annogram
+
+    //char W1[] = "весна";
+    //char W2[] = "навес"; //Вывод: annogram
 
 
-    if (!fill_tab(W1, Tab)) {
-        printf("Invalid pointer");
-        return 0;
-    }
+    //char W1[] = "aab";        // Латинские
+    //char W2[] = "ааb";        // Кириллические а + латинская b //Вывод: not an annogram
 
-    if (check_annogram(W1,W2,Tab)) {
+    //char W1[] = "google";     // Латинские символы
+    //char W2[] = "gооgle";     // С кириллическими о //Вывод: not an annogram
+
+    //char W1[] = "a";          // Латинская a
+    //char W2[] = "а";          // Кириллическая а //Вывод: not an annogram
+
+     //char W1[] = "ÀÿАп";
+     //char W2[] = "ÀÀпп"; //Вывод: not an annogram
+
+    //W1: C3=2, 80=1, BF=2, D0=2, 90=1
+    //W2: C3 = 2, 80 = 2, BF = 2, D0 = 2
+
+
+
+
+    if (check_annogram(W1, W2)) {
         printf("annogram");
     }
     else {
